653b1ddc5775da5185b9adea2682261fda4920c4
Harish <harish.workx@gmail.com>
2026-06-23 16:51:55 +0530

    Set BASE_URL for sitemap generation

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 import { services, serviceAreas } from "@/data/business";
 
-const BASE_URL = "";
+const BASE_URL = "https://mwebbplumbingco.com";
 
 export const Route = createFileRoute("/sitemap.xml")({
   server: {
